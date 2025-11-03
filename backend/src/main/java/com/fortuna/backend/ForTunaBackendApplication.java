84c7b247ae75aa82cc8d62699a7d588e8e8dbe9c
Edis Herrmann <wi24045@lehre.dhbw-stuttgart.de>
2025-11-03 08:37:07 +0100

    Initial commit, angular and springboot structure

---
+++ backend/src/main/java/com/fortuna/backend/ForTunaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.fortuna.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForTunaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForTunaBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/fortuna/backend/ForTunaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fortuna.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForTunaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
